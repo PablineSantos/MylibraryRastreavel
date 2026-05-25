be62bfc2a9b0f2e403e3811bb624e4baa0ffac8c
PablineSantos <pablines2006@gmail.com>
2026-05-25 15:06:13 -0300

    feat(backend): criar entidade Livro e Enum Status (#2)

---
+++ backend/src/main/java/com/pabline/senai/backend/entity/Livro.java
@@ -0,0 +1,101 @@
+package com.pabline.senai.backend.entity;
+
+import com.pabline.senai.backend.enums.Status;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String autor;
+
+    @Column(unique = true)
+    private String isbn;
+
+    @Column
+    private int ano;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
+
+    @OneToMany(mappedBy = "livro", cascade = CascadeType.ALL)
+    private List<Emprestimo> emprestimos = new ArrayList<>();
+
+    public Livro() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public List<Emprestimo> getEmprestimos() {
+        return emprestimos;
+    }
+
+    public void setEmprestimos(List<Emprestimo> emprestimos) {
+        this.emprestimos = emprestimos;
+    }
+}
---
+++ backend/src/main/java/com/pabline/senai/backend/enums/Status.java
@@ -0,0 +1,5 @@
+package com.pabline.senai.backend.enums;
+
+public enum Status {
+    DISPONIVEL, EMPRESTADO
+}
